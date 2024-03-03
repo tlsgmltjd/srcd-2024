b848b4e2095d05673ead537a864d7725d00395df
신희성 <vvwv3092@gmail.com>
2024-03-04 02:03:08 +0900

    Add header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import HeaderLogo from "../assets/HeaderLogo";
 
 function Header() {
   return (
     <HeaderContainer>
-      <HeaderTitle>GENESIS</HeaderTitle>
+      <HeaderTitle>
+        <HeaderLogo />
+      </HeaderTitle>
       <HeaderBtnBox>
         <HeaderBtn>
           <Link to={"/"}>시연회 소개</Link>
         </HeaderBtn>
-
         <HeaderBtn>
           <Link to={"/clubs"}>동아리 목록</Link>
         </HeaderBtn>
-
         <HeaderBtn>
           <Link to={"/faq"}>FAQ</Link>
         </HeaderBtn>
@@ -25,7 +26,13 @@ function Header() {
 
 export default Header;
 
-const HeaderContainer = styled.header``;
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  padding: 5px 0;
+`;
 
 const HeaderTitle = styled.h1``;
 
