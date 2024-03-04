ace4d98b7bffff43448f62f3d4dd6c753fd25aa5
신희성 <vvwv3092@gmail.com>
2024-03-04 09:49:41 +0900

    Add club box layout

---
+++ src/assets/LeftArrowIconBlack.jsx
@@ -0,0 +1,36 @@
+function LeftArrowIconBlack() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="29"
+      height="29"
+      viewBox="0 0 29 29"
+      fill="none"
+    >
+      <mask
+        id="mask0_335_4696"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="29"
+        height="29"
+      >
+        <rect
+          x="0.388184"
+          y="0.0273438"
+          width="28.6118"
+          height="28.6118"
+          fill="#D9D9D9"
+        />
+      </mask>
+      <g mask="url(#mask0_335_4696)">
+        <path
+          d="M16.895 21.0735L15.6387 19.7805L20.1918 15.2274H5.75293V13.4392H20.1918L15.6387 8.88603L16.895 7.59302L23.6352 14.3333L16.895 21.0735Z"
+          fill="#1C1B1F"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default LeftArrowIconBlack;
--- src/pages/Clubs.jsx
+++ src/pages/Clubs.jsx
@@ -1,7 +1,24 @@
 import styled from "styled-components";
+import LeftArrowIconBlack from "../assets/LeftArrowIconBlack";
 
 function Clubs() {
-  return <ClubContainer></ClubContainer>;
+  return (
+    <ClubContainer>
+      <ClubTitleBox></ClubTitleBox>
+      <ClubBox>
+        <ClubConentBox>
+          <ClubContentTitleBox>
+            <ClubContentTitle>v.friends</ClubContentTitle>
+            <ClubContent>디자인 교육 봉사 동아리</ClubContent>
+          </ClubContentTitleBox>
+          <ClubContent>콘텐츠디자인과</ClubContent>
+        </ClubConentBox>
+        <ClubBottomBox>
+          <LeftArrowIconBlack />
+        </ClubBottomBox>
+      </ClubBox>
+    </ClubContainer>
+  );
 }
 
 export default Clubs;
@@ -13,4 +30,22 @@ const ClubContainer = styled.main`
   grid-template-rows: repeat(3, minmax(450px, 1fr));
 `;
 
-const ClubBox = styled.div``;
+const ClubBox = styled.div`
+  background-color: red;
+`;
+
+const ClubConentBox = styled.div``;
+
+const ClubContentTitleBox = styled.div``;
+
+const ClubContentTitle = styled.h1``;
+
+const ClubContent = styled.p``;
+
+const ClubBottomBox = styled.div``;
+
+const ClubTitleBox = styled.div`
+  background-color: #0d00ff;
+  grid-column: 1 / 3;
+  grid-row: 1 / 2;
+`;
